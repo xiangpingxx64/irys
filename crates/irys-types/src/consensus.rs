f9b6574ef3bd0c0bb04f35f8493987c995696b20
dmac <samuraidan@gmail.com>
2024-10-19 14:42:24 -0700

    feat: add VDF consensus params

--- crates/irys-types/src/consensus.rs
+++ crates/irys-types/src/consensus.rs
@@ -3,3 +3,12 @@ pub const PARTITION_SIZE: u64 = CHUNK_SIZE * 10;
 pub const BLOCK_TIME: u64 = 30_000;
 
 pub const CHUNK_SIZE: u64 = 256 * 1024;
+
+// Reset the nonce limiter (vdf) once every 1200 steps/seconds or every ~20 min
+pub const NONCE_LIMITER_RESET_FREQUENCY: usize = 10 * 120;
+
+// 25 checkpoints 40 ms each = 1000 ms
+pub const NUM_CHECKPOINTS_IN_VDF_STEP: usize = 25;
+
+// Typical ryzen 5900X iterations for 1 sec
+pub const VDF_SHA_1S: u64 = 15_000_000;
