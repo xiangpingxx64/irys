85b2c557b4231244242660f314c87e3ba657dbd0
joshbenaron <73971531+joshbenaron@users.noreply.github.com>
2024-10-20 23:54:32 +0400

    Fix proxy

--- crates/api-server/src/routes/proxy.rs
+++ crates/api-server/src/routes/proxy.rs
@@ -1,7 +1,11 @@
-use actix_web::{web::Data, HttpResponse};
-use awc::Client;
+use actix_web::{web::{Data, Payload}, HttpRequest, HttpResponse};
+use awc::{http::Method, Client};
 use actix_proxy::{IntoHttpResponse, SendRequestError};
 
-pub async fn proxy(client: Data<Client>) -> Result<HttpResponse, SendRequestError> {
-    Ok(client.get("http://localhost:4567").send().await?.into_http_response())
+pub async fn proxy(req: HttpRequest, payload: Payload, client: Data<Client>) -> Result<HttpResponse, SendRequestError> {
+    match *req.method() {
+        Method::GET => Ok(client.get("http://localhost:8545").send().await?.into_http_response()),
+        Method::POST => Ok(client.post("http://localhost:8545").send_stream(payload).await?.into_http_response()),
+        _ => Ok(HttpResponse::MethodNotAllowed().finish())
+    }
 }
