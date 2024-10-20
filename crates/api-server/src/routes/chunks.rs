745e72fe91037a20e394fe06ec33e3f7d1f71e29
joshbenaron <73971531+joshbenaron@users.noreply.github.com>
2024-10-20 21:20:50 +0400

    Implement POST /tx

--- crates/api-server/src/routes/chunks.rs
+++ crates/api-server/src/routes/chunks.rs
@@ -1,11 +1,12 @@
 use actix_web::{web::Json, HttpResponse};
+use irys_types::Base64;
 use serde::Deserialize;
 
 // TODO: Implement serde decoding for Base64url
 #[derive(Deserialize)]
 pub struct PostChunkBody {
-    chunk: String,
-    data_path: String
+    chunk: Base64,
+    data_path: Base64
 }
 
 pub async fn post_chunk(chunk: Json<PostChunkBody>) -> actix_web::Result<HttpResponse> {
