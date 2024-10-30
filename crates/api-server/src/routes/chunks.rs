284eee4b289ba47b0cb702ec2329a07b8e013cc2
dmac <samuraidan@gmail.com>
2024-10-30 09:35:09 -0700

    doc: add important chunk field documentation

--- crates/api-server/src/routes/chunks.rs
+++ crates/api-server/src/routes/chunks.rs
@@ -1,15 +1,8 @@
 use actix_web::{web::Json, HttpResponse};
-use irys_types::Base64;
+use irys_types::{Base64, Chunk};
 use serde::Deserialize;
 
-// TODO: Implement serde decoding for Base64url
-#[derive(Deserialize)]
-pub struct PostChunkBody {
-    chunk: Base64,
-    data_path: Base64,
-}
-
-pub async fn post_chunk(chunk: Json<PostChunkBody>) -> actix_web::Result<HttpResponse> {
+pub async fn post_chunk(chunk: Json<Chunk>) -> actix_web::Result<HttpResponse> {
     // TODO: Implement logic
 
     Ok(HttpResponse::Ok().finish())
--- crates/types/src/chunk.rs
+++ crates/types/src/chunk.rs
@@ -4,9 +4,21 @@ use crate::{Base64, H256};
 
 #[derive(Serialize, Deserialize, Debug, Default, PartialEq)]
 pub struct Chunk {
+    /// The root hash for this chunk which should map to the root_hash in the
+    /// transaction header. Having this present makes it easier to do cached
+    /// chunk lookups by data_root on the ingress node.
     pub data_root: H256,
+    /// Total size of the data this chunk belongs to. Helps identify if this
+    /// is the last chunk in the transactions data, or one that comes before it.
+    /// Only the last chunk can be smaller than CHUNK_SIZE.
     pub data_size: u64,
+    /// Raw bytes of the merkle proof that connects the data_root and the
+    /// chunk hash
     pub data_path: Base64,
+    /// Raw bytes to be stored, should be CHUNK_SIZE in length unless it is the
+    /// last chunk in the transaction
     pub bytes: Base64,
+    /// Offset of the chunk in the transactions data. Offsets are measured from
+    /// the highest (last) byte in the chunk not the first.
     pub offset: usize,
 }
