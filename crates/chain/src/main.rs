afdc2974f7f0b914b179e4f71a1724e3e3f24cd7
JesseTheRobot <jesse.cruz.wright@gmail.com>
2024-12-17 14:32:52 +0000

    misc: change default log level for executable to Debug

--- crates/chain/src/main.rs
+++ crates/chain/src/main.rs
@@ -17,7 +17,7 @@ async fn main() -> eyre::Result<()> {
     // TODO: fix this, we used to await the reth node exit future but can't anymore
     // so we need another near-infinite blocking future
     let _ = SubscriberBuilder::default()
-        .with_max_level(LevelFilter::INFO)
+        .with_max_level(LevelFilter::DEBUG)
         .finish()
         .try_init();
 
