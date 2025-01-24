358dfe3ad674deb837708eca865747a034b1facb
Sam Johnson <sam@durosoft.com>
2025-01-24 15:57:37 -0500

    take away need to cargo clean when changing testnet.toml (#140)

--- crates/types/build.rs
+++ crates/types/build.rs
@@ -3,6 +3,7 @@ use std::env;
 
 fn main() {
     println!("cargo::rerun-if-env-changed=IRYS_ENV");
+    println!("cargo::rerun-if-changed=crates/types/configs");
     let Ok(irys_env) = env::var("IRYS_ENV") else {
         info!("irys-types using default config (set IRYS_ENV to override)");
         return;
