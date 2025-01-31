169dc8d9a43086a66814b8a38ab18db6e533f3d8
JesseTheRobot <jesse.cruz.wright@gmail.com>
2025-01-31 23:06:33 +0000

    fix: remove debug-specific VDF difficulty adjustment

--- crates/types/src/vdf_config.rs
+++ crates/types/src/vdf_config.rs
@@ -18,11 +18,7 @@ impl Default for VDFStepsConfig {
         VDFStepsConfig {
             num_checkpoints_in_vdf_step: CONFIG.num_checkpoints_in_vdf_step,
             vdf_reset_frequency: CONFIG.vdf_reset_frequency,
-            vdf_difficulty: if cfg!(test) || cfg!(debug_assertions) {
-                7_000
-            } else {
-                CONFIG.vdf_sha_1s
-            },
+            vdf_difficulty: CONFIG.vdf_sha_1s,
             vdf_parallel_verification_thread_limit: CONFIG.vdf_parallel_verification_thread_limit,
         }
     }
