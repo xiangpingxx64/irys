1cdcf208db6bf8b8dc5cc3d8f6011c14e566385a
JesseTheRobot <jesse.cruz.wright@gmail.com>
2025-02-01 21:51:43 +0000

    fix: incorrect macro path causing constant recompilation

--- crates/macros/build.rs
+++ crates/macros/build.rs
@@ -1,5 +1,5 @@
 fn main() {
     println!("cargo::rerun-if-env-changed=IRYS_ENV");
     println!("cargo::rerun-if-env-changed=CONFIG_TOML_PATH");
-    println!("cargo::rerun-if-changed=crates/types/configs");
+    println!("cargo::rerun-if-changed=../../crates/types/configs");
 }
