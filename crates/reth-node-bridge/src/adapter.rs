e5e484259547212329dd2c89f5235a9e4fa3d3b0
JesseTheRobot <jesse.cruz.wright@gmail.com>
2025-06-11 21:20:10 +0000

    fix: Reth payload jobs from living too long
    Use of `best_payload` caused reth payloads for previous blocks to live until their maximum deadline, resulting in unneeded resource utilisation. resolve_kind however ends up in a Reth codepath that does mean resolved payloads are killed as expected.

--- crates/reth-node-bridge/src/adapter.rs
+++ crates/reth-node-bridge/src/adapter.rs
@@ -4,6 +4,7 @@ use std::{
     time::{SystemTime, UNIX_EPOCH},
 };
 
+use crate::node::{eth_payload_attributes, RethNode};
 use crate::node::{RethNodeAdapter, RethNodeAddOns};
 use alloy_eips::BlockNumberOrTag;
 use alloy_primitives::{BlockNumber, B256};
@@ -17,9 +18,8 @@ use reth::transaction_pool::EthPooledTransaction;
 use reth_e2e_test_utils::node::NodeTestContext;
 use reth_node_api::{EngineApiMessageVersion, NodeTypes, PayloadTypes};
 use reth_payload_builder::EthPayloadBuilderAttributes;
+use reth_payload_builder::PayloadKind;
 use reth_provider::BlockReaderIdExt as _;
-
-use crate::node::{eth_payload_attributes, RethNode};
 
 #[derive(Clone)]
 pub struct IrysRethNodeAdapter {
@@ -160,7 +160,7 @@ impl IrysRethNodeAdapter {
             .reth_node
             .payload
             .payload_builder
-            .best_payload(payload_id)
+            .resolve_kind(payload_id, PayloadKind::WaitForPending)
             .await
             .unwrap()?;
         Ok(payload)
