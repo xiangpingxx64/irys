fc91c2a4807c0f4411eff62b9b639de6b93de835
JesseTheRobot <jesse.cruz.wright@gmail.com>
2025-07-08 23:35:46 +0000

    fix: H256 Display impl

--- crates/types/src/h256.rs
+++ crates/types/src/h256.rs
@@ -170,7 +170,11 @@ impl fixed_hash::core_::fmt::Debug for H256 {
 }
 impl fixed_hash::core_::fmt::Display for H256 {
     fn fmt(&self, f: &mut fixed_hash::core_::fmt::Formatter) -> fixed_hash::core_::fmt::Result {
-        write!(f, "{}", &&self.0.to_base58())
+        let s = self.0.to_base58();
+        write!(f, "{}", &s[0..4])?;
+        f.write_fmt(core::format_args!("…"))?;
+        write!(f, "{}", &s[s.len() - 4..])?;
+        Ok(())
     }
 }
 impl fixed_hash::core_::fmt::LowerHex for H256 {
